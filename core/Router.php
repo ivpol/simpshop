7bba8ea6221ca82bd7d7f0040ec6186b7197e0b8
ivpol <ivpol@ivpol.com>
2017-09-07 20:44:50 +0300

    Исправлен роутинг (теперь всё через .htaccess и $_GET)

--- core/Router.php
+++ core/Router.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Core;
 
 /**
@@ -6,134 +7,66 @@ namespace Core;
  */
 class Router
 {
-    private $route; //оригинальный адрес
+    private $urlParams;
 
-    private $mask; //маска для разбивки адреса на переменные (разделители - . и /)
+    private $controller;
 
-    private $controller; //массив с названием текущего контроллера, его методом и параметрами
-
-    //при создании задаётся маршрут и маска
-    public function __construct($route = '/', $mask = '')
+    public function __construct(Array $urlParams = [])
     {
-        $this->controller = ['name' => '', 'action' => '', 'params' => []];
-        $this->setRoute($route, $mask);
-
-    }
-
-    //установка маршрута
-    public function setRoute($route, $mask = '')
-    {
-        $route = trim(strtolower($route));
-        if ($route) {
-            $this->route = $route;
-        } else {
-            $route = '/';
+        $this->controller = ['name' => '', 'method' => '', 'params' => []];
+        if ($urlParams) {
+            $this->setRoute($urlParams);
         }
-        $this->mask = trim($mask);
-        $this->setController();//задаются актуальные данные о контроллере
     }
 
-    //установка данных контроллера
-    private function setController()
+    private function setRoute(Array $urlParams)
     {
-        $controller = [];
-        $routeData = preg_split('/\//', $this->route, 2, PREG_SPLIT_NO_EMPTY); //строка запроса разбивается на две, по разделителю, пустые не нужны
-        if (isset($routeData[0]) && $this->isNameCorrect($routeData[0])) { //если первая подстрока есть и не содержит некорректных символов
-            $controller['name'] = $routeData[0]; //первая подстрока - название контроллера
-            if (isset($routeData[1])) { //если есть вторая подстрока, она должна содержать название метода и параметры
-                $paramsPosition = strpos($routeData[1], '?'); //позиция начала строки с параметрами
-                if ($paramsPosition !== 0) { // если строка с данными пути не начинается с параметров, а содержит как минимум название метода
-                    $paramsString = '';//параметры для метода контроллера, которые переданы в формате get
-                    if ($paramsPosition > 0) { // если строка с параметрами вообще есть
-                        $routeData = explode('?', $routeData[1], 2); //данные о пути теперь содержат строку с названием метода (и, если есть, параметрами для маски) и строку с параметрами
-                        $paramsString = $routeData[1];
-                    } else { //если строки с параметрами нет
-                        $routeData = [$routeData[1]];//данные о пути содержат только одну строку - с названием метода и, если есть, парметрами для маски
-                    }
-                    // данные о пути теперь массив с двумя строками
-                    // (первая - название метода котроллера, вторая - строка параметров, которые передаются через маску)
-                    $routeData = preg_split('/[\/\.]/', $routeData[0], 2);
-                    if ($routeData[0] && $this->isNameCorrect($routeData[0])) { //если название контроллера корректно
-                        $controller['action'] = $routeData[0];
-                        $params = [];//общий массив, в который добавляются все параметры
-                        if ($paramsString) {//если есть параметры в формате get-запроса
-                            parse_str($paramsString, $params);
-                        }
-                        // TODO добавить расширенный функционал для создания масок адресов (напр. *.par)
-                        if ($this->mask) {// если есть маска
-                            $key = '';
-                            $value = '';
-                            //получаем полный массив, где названия переменных разбиты разделителями (если пустой - соответствующее значение из строки запроса не нужно)
-                            $paramsKeys = preg_split('/[\/\.]/', $this->mask);
-                            //массив из строки, разбитый по разделителям
-                            $paramsValues = [];
-                            if (!empty($routeData[1])) {
-                                $paramsValues = preg_split('/[\/\.]/', $routeData[1]);
-                            }
-                            while ($key = array_shift($paramsKeys)) {
-                                if ($key) {//если непустое название ключа, то ему задаётся соответствующее значение из строки
-                                    if (isset($paramsValues[0])) {
-                                        $value = array_shift($paramsValues);
-                                    } else {
-                                        $value = '';
-                                    }
-                                    $params[$key] = $value;
-                                }
-                            }
-                        }
-                        $controller['params'] = $params;
-                        $this->controller = $controller;
-                    }
+        if (!empty($urlParams['_module_'])) {
+            $this->controller['name'] = $urlParams['_module_'];
+
+        }
+        if (!empty($urlParams['_action_'])) {
+            $this->controller['method'] = $urlParams['_action_'];
+        }
+        if (!empty($urlParams['_params_'])) {
+            $params = explode('/', $urlParams['_params_']);
+            array_walk($params, function (&$param) {
+                if (strpos($param, '.') !== false) {
+                    $param = explode('.', $param);
                 }
-            }
+            });
+            $this->controller['params'] = $params;
+        }
+        unset($urlParams['_module_'], $urlParams['_params_'], $urlParams['_action_']);
+        if (!empty($urlParams)) {
+            $this->controller['params'] += $urlParams;
         }
     }
 
-
-    //возвращает строку с названием контроллера, который запрашивается
     public function getControllerName()
     {
         return $this->controller['name'];
     }
 
-    //возвращает строку с названиме запрашиваемого метода
-    public function getAction()
+    public function getMethod()
     {
-        return $this->controller['action'];
+        return $this->controller['method'];
     }
 
-    //возвращает преобразованную в массив строку запроса после ?
     public function getParams()
     {
         return $this->controller['params'];
     }
 
-    //возвращает строку, сгенерированную из названий контроллера и метода и массива параметров
     public function getRoute($controller = '', $action = '', Array $params = [])
     {
 
     }
 
-    //преобразование массива в строку запроса
     private function getParamsString(Array $params = [])
     {
         if ($params) {
             return http_build_query($params);
-        }
-    }
-
-
-    //проверка названий контроллеров и методов на корректность
-    private function isNameCorrect($name)
-    {
-        if ($name) {
-            if (preg_match('/[^a-z]/i', $name)) {
-                return false;
-            } else {
-                return true;
-            }
-        } else {
-            return false;
         }
     }
 
--- public/index.php
+++ public/index.php
@@ -1,42 +1,27 @@
 <?php
 
-if (!defined('ROOT_DIR')) {
-    define('ROOT_DIR', realpath(__DIR__ . '/../'));
+$rootDir = realpath(__DIR__ . '/../');
+
+include_once $rootDir . '/Core/Autoloader.php';
+$autoloader = new Core\Autoloader($rootDir);
+$autoloader->loadFromDir('Core/utils');
+
+$urlParams = $_GET;
+
+if (empty($urlParams['_module_'])) {
+    $urlParams['_module_'] = 'main';
 }
 
-//Загрузка конфигурационных настроек сайта
-include_once ROOT_DIR . '/core/Config.php';
-// TODO добавить статику в конфиг
-$config = new Core\Config;
-
-include_once ROOT_DIR . '/core/Autoloader.php';
-$autoloader = new Core\Autoloader(ROOT_DIR);
-$autoloader->loadFromDir('core/utils');
-
-//маршрутизация
-$route = '';
-
-if ($_SERVER['REQUEST_URI'] != '/') {
-    $route = $_SERVER['REQUEST_URI'];
-} else {
-    //дефолтный маршрут, если в корне
-    $route = '/main/index';
-}
-
-$router = new Core\Router($route, 'content_type');
+$router = new Core\Router($urlParams);
 
 $controllerName = $router->getControllerName();
 if ($controllerName && class_exists('App\\Controllers\\' . $controllerName)) {
     $controllerName = 'App\\Controllers\\' . $controllerName;
     $controller = new $controllerName();
-    $action = $router->getAction();
-    if ($action && is_callable([$controller, $action])) {
-         $controller->$action($router->getParams());
-    } else {
-        // TODO 404
-        echo 'Метод не существует<br>';
+    $method = $router->getMethod();
+    if (is_callable([$controller, $method])) {
+         $controller->$method($router->getParams());
     }
-
 } else {
     // TODO 404
     echo 'Класс не существует<br>';
